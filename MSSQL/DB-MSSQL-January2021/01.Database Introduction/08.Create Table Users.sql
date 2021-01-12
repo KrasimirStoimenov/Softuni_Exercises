920bd460ca55f4a7c95485e0a91b29645a9d8955
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-01-12 11:45:33 +0200

    Create 08.Create Table Users.sql

---
+++ MSSQL/DB-MSSQL-January2021/01.Database Introduction/08.Create Table Users.sql
@@ -0,0 +1,16 @@
+----- Problem 8.Create Table Users
+CREATE TABLE Users
+(	Id BIGINT PRIMARY KEY IDENTITY,
+	Username VARCHAR(30) NOT NULL,
+	[Password] VARCHAR(26) NOT NULL,
+	ProfilePicture VARBINARY(MAX),
+	LastLoginTime DATETIME2,
+	IsDeleted VARCHAR(5)
+)
+
+INSERT INTO Users (Username,[Password])
+VALUES  ('FirstTest' , 'FirstTestPassword'),
+		('SecondTest', 'SomePassword'),
+		('ThirdTest', 'pAsSwOrD'),
+		('FourthTest', 'something'),
+		('FifthTest', 'pass123')
