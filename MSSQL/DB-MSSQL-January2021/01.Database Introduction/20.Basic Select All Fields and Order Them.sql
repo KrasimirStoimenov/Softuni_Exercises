f135138aec4c102aea30d9de70d8c30b6d4aabc3
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-01-13 14:21:19 +0200

    Create 20.Basic Select All Fields and Order Them.sql

---
+++ MSSQL/DB-MSSQL-January2021/01.Database Introduction/20.Basic Select All Fields and Order Them.sql
@@ -0,0 +1,10 @@
+-----Problem 20.Basic Select All Fields and Order Them
+
+SELECT * FROM Towns
+ORDER BY Towns.[Name] ASC
+
+SELECT * FROM Departments
+ORDER BY Departments.[Name] ASC
+
+SELECT * FROM Employees
+ORDER BY Employees.Salary DESC
