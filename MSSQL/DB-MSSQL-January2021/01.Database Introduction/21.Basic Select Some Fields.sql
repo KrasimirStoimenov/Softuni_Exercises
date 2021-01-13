6e109dcafccaaece087fbaf60380c6da9ef98cdd
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-01-13 14:21:21 +0200

    Create 21.Basic Select Some Fields.sql

---
+++ MSSQL/DB-MSSQL-January2021/01.Database Introduction/21.Basic Select Some Fields.sql
@@ -0,0 +1,10 @@
+-----Problem 21.Basic Select Some Fields
+
+SELECT [Name] FROM Towns
+ORDER BY Towns.[Name] ASC
+
+SELECT [Name] FROM Departments
+ORDER BY Departments.[Name] ASC
+
+SELECT FirstName,LastName,JobTitle,Salary FROM Employees
+ORDER BY Employees.Salary DESC
