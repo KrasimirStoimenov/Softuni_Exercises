91e89357e7e709838a43cde0a43d6fe4f6de446f
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-01-19 10:03:34 +0200

    Create 06.University Database.sql

---
+++ MSSQL/DB-MSSQL-January2021/03.Table Relations/06.University Database.sql
@@ -0,0 +1,40 @@
+--Problem 6.University Database
+
+CREATE DATABASE University
+
+USE University
+
+CREATE TABLE Majors
+(
+	MajorID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	StudentNumber CHAR(10) NOT NULL,
+	StudentName VARCHAR(50) NOT NULL,
+	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments
+(
+	PaymentID INT PRIMARY KEY IDENTITY,
+	PaymentDate DATE NOT NULL,
+	PaymentAmount DECIMAL (9,2) NOT NULL,
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects
+(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName VARCHAR(50)
+)
+
+CREATE TABLE Agenda
+(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID),
+	PRIMARY KEY (StudentID, SubjectID)
+)
