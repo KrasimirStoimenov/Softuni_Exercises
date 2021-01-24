e9f76be432372e8663587c88fc5c91ba33c84a89
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-01-24 10:15:48 +0200

    Create 07.Employees with Project.sql

---
+++ MSSQL/DB-MSSQL-January2021/05.Subqueries And Joins/07.Employees with Project.sql
@@ -0,0 +1,8 @@
+--7.Employees with Project
+
+SELECT TOP(5) e.EmployeeID,e.FirstName,p.[Name] AS ProjectName
+FROM Employees AS e
+LEFT OUTER JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+LEFT OUTER JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13' AND p.EndDate IS NULL
+ORDER BY e.EmployeeID
