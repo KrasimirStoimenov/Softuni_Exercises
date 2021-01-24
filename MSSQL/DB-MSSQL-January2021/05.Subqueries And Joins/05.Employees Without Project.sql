a85ec06c342a6b75129708f3b091d37f81ebf6a4
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-01-24 10:15:44 +0200

    Create 05.Employees Without Project.sql

---
+++ MSSQL/DB-MSSQL-January2021/05.Subqueries And Joins/05.Employees Without Project.sql
@@ -0,0 +1,8 @@
+--5.Employees Without Project
+
+SELECT TOP(3) e.EmployeeID,e.FirstName 
+FROM Employees AS e
+LEFT OUTER JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+LEFT OUTER JOIN Projects AS p ON ep.ProjectID=p.ProjectID
+WHERE p.ProjectID IS NULL
+ORDER BY e.EmployeeID
