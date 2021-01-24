7a7df67169652c7751130fa39334547b2e242562
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-01-24 11:00:42 +0200

    Update 05.Employees Without Project.sql

--- MSSQL/DB-MSSQL-January2021/05.Subqueries And Joins/05.Employees Without Project.sql
+++ MSSQL/DB-MSSQL-January2021/05.Subqueries And Joins/05.Employees Without Project.sql
@@ -3,6 +3,5 @@
 SELECT TOP(3) e.EmployeeID,e.FirstName 
 FROM Employees AS e
 LEFT OUTER JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
-LEFT OUTER JOIN Projects AS p ON ep.ProjectID=p.ProjectID
-WHERE p.ProjectID IS NULL
+WHERE ep.ProjectID IS NULL
 ORDER BY e.EmployeeID
