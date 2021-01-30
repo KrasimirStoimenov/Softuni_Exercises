81f34a637abb7c24b725a2cf650c4c0b65e00dce
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-01-30 16:09:38 +0200

    Create 02.Employees with Salary Above Number.sql

---
+++ MSSQL/DB-MSSQL-January2021/07.Functions and Stored Procedures/02.Employees with Salary Above Number.sql
@@ -0,0 +1,11 @@
+--2.Employees with Salary Above Number
+
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber (@minSalary DECIMAL(18,4))
+AS
+BEGIN 
+		SELECT FirstName,LastName 
+		FROM Employees
+		WHERE Salary >= @minSalary
+END
+--------------------------------------------------
+EXEC dbo.usp_GetEmployeesSalaryAboveNumber 48100
