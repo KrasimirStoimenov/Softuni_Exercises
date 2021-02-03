2030d9569e12e379f21298164a1c8b193401ac55
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-02-03 14:40:33 +0200

    Update 07.Count colonists.sql

--- MSSQL/DB-MSSQL-January2021/Exam Preparation/Databases MSSQL Server Exam - 28 Jun 2020/07.Count colonists.sql
+++ MSSQL/DB-MSSQL-January2021/Exam Preparation/Databases MSSQL Server Exam - 28 Jun 2020/07.Count colonists.sql
@@ -1,8 +1,8 @@
 --7.Count colonists
 
 SELECT COUNT(*) AS count
-FROM Colonists c
-JOIN TravelCards tc ON tc.ColonistId = c.Id
-JOIN Journeys j ON j.Id = tc.JourneyId
+	FROM Colonists c
+	JOIN TravelCards tc ON tc.ColonistId = c.Id
+	JOIN Journeys j ON j.Id = tc.JourneyId
 GROUP BY j.Purpose
-HAVING j.Purpose = 'Technical'
+	HAVING j.Purpose = 'Technical'
