5311b7d3f17af1325a4992d9d442ee79f7a64dd5
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-02-04 09:41:08 +0200

    Create 11.Get Colonists Count.sql

---
+++ MSSQL/DB-MSSQL-January2021/Exam Preparation/Databases MSSQL Server Exam - 28 Jun 2020/11.Get Colonists Count.sql
@@ -0,0 +1,18 @@
+--11.Get Colonists Count
+
+CREATE FUNCTION udf_GetColonistsCount(@planetName VARCHAR (30))
+RETURNS INT
+AS
+BEGIN
+		RETURN (SELECT COUNT(tc.ColonistId) AS [Count] 
+						FROM Planets p 
+						JOIN Spaceports sp ON sp.PlanetId = p.Id
+						JOIN Journeys j ON j.DestinationSpaceportId = sp.Id
+						JOIN TravelCards tc ON tc.JourneyId = j.Id					
+					  GROUP BY p.[Name]
+						HAVING p.[Name] = @planetName)		
+END
+
+GO
+
+SELECT dbo.udf_GetColonistsCount('Otroyphus')
