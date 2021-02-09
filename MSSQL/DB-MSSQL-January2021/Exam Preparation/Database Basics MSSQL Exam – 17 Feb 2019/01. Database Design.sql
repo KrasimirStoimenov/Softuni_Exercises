c65f22b8bf0ea09fddbdb4074371fb60790abf30
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-02-09 13:44:22 +0200

    Create 01. Database Design.sql

---
+++ MSSQL/DB-MSSQL-January2021/Exam Preparation/Database Basics MSSQL Exam – 17 Feb 2019/01. Database Design.sql
@@ -0,0 +1,58 @@
+--1. Database Design
+
+CREATE TABLE Students
+(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(30) NOT NULL,
+	MiddleName NVARCHAR(25),
+	LastName NVARCHAR(30) NOT NULL,
+	Age SMALLINT CHECK(Age>0),
+	[Address] NVARCHAR(50),
+	Phone NCHAR(10)
+)
+
+CREATE TABLE Subjects
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(20) NOT NULL,
+	Lessons INT NOT NULL CHECK (Lessons >0)
+)
+
+CREATE TABLE StudentsSubjects
+(
+	Id INT PRIMARY KEY IDENTITY,
+	StudentId INT NOT NULL REFERENCES Students(Id),
+	SubjectId INT NOT NULL REFERENCES Subjects(Id),
+	Grade DECIMAL(3,2) NOT NULL CHECK(Grade BETWEEN 2.00 AND 6.00)
+)
+
+CREATE TABLE Exams
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Date] DATETIME,
+	SubjectId INT NOT NULL REFERENCES Subjects(Id)
+)
+
+CREATE TABLE StudentsExams
+(
+	StudentId INT NOT NULL REFERENCES Students(Id),
+	ExamId INT NOT NULL REFERENCES Exams(Id),
+	PRIMARY KEY (StudentId,ExamId),
+	Grade DECIMAL(3,2) NOT NULL CHECK(Grade BETWEEN 2.00 AND 6.00)
+)
+CREATE TABLE Teachers
+(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(20) NOT NULL,
+	LastName NVARCHAR(20) NOT NULL,
+	[Address] NVARCHAR(20) NOT NULL,
+	Phone CHAR(10),
+	SubjectId INT NOT NULL REFERENCES Subjects(Id)
+)
+
+CREATE TABLE StudentsTeachers
+(
+	StudentId INT NOT NULL REFERENCES Students(Id),
+	TeacherId INT NOT NULL REFERENCES Teachers(Id)
+	PRIMARY KEY (StudentId,TeacherId)
+)
