cbf5a6d8145d1cf3294582f61a1405ba4947ccf5
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-02-12 08:23:52 +0200

    Create 07.Passenger Trips.sql

---
+++ MSSQL/DB-MSSQL-January2021/Exam Preparation/Database Basics MS SQL Exam – 16 Apr 2019/07.Passenger Trips.sql
@@ -0,0 +1,9 @@
+--7.	Passenger Trips
+
+SELECT CONCAT(FirstName,' ',LastName) AS [Full Name],
+	   f.Origin,
+	   f.Destination
+	FROM Passengers p
+	JOIN Tickets t ON  t.PassengerId = p.Id
+	JOIN Flights f ON t.FlightId = f.Id
+ORDER BY [Full Name],f.Origin,f.Destination
