7a9b2d1e274495385762c78a80e1c51dd393c969
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-16 19:04:55 +0200

    Create Program.cs

---
+++ C#/Entity Framework Core - February 2021/01.ADO.NET/01. Initial Setup/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           SqlConnection connection = new SqlConnection("Server = .; Database = MinionsDB; Integrated Security = True;");
+            connection.Open();
+            connection.Close();
+        }
+    }
+}
