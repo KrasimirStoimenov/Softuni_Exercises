f405280e1c8273d8a93f08aa788ce641ce4c1443
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-17 20:01:10 +0200

    Update Program.cs

--- C#/Entity Framework Core - February 2021/01.ADO.NET/02. Villain Names/Program.cs
+++ C#/Entity Framework Core - February 2021/01.ADO.NET/02. Villain Names/Program.cs
@@ -8,32 +8,27 @@ namespace _02._Villain_Names
         static void Main(string[] args)
         {
             string connectionString = "Server = .; Database = MinionsDB; Integrated Security = True;";
-            SqlConnection dbCon = new SqlConnection(connectionString);
+            using var dbCon = new SqlConnection(connectionString);
 
             dbCon.Open();
 
-            using (dbCon)
+            using var query = new SqlCommand(@"SELECT v.[Name], COUNT(mv.MinionId) AS MinionsCount
+                                       FROM Villains v
+                                       JOIN MinionsVillains mv ON mv.VillainId = v.Id
+                                       GROUP BY v.[Name]
+                                       HAVING COUNT(mv.MinionId) > 3
+                                       ORDER BY MinionsCount", dbCon);
+
+            using var reader = query.ExecuteReader();
+
+                while (reader.Read())
             {
-                var query = new SqlCommand(" SELECT v.[Name], COUNT(mv.MinionId) AS MinionsCount" +
-                                           " FROM Villains v" +
-                                           " JOIN MinionsVillains mv ON mv.VillainId = v.Id" +
-                                           " GROUP BY v.[Name]" +
-                                           " HAVING COUNT(mv.MinionId) > 3" +
-                                           " ORDER BY MinionsCount", dbCon);
+                string villainName = reader["Name"] as string;
+                int? minionsCount = reader["MinionsCount"] as int?;
 
-                using (var reader = query.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        string villainName = reader["Name"] as string;
-                        int? minionsCount = reader["MinionsCount"] as int?;
-
-                        Console.WriteLine($"{villainName} - {minionsCount}");
-                    }
-                }
-
-
+                Console.WriteLine($"{villainName} - {minionsCount}");
             }
         }
+
     }
 }
