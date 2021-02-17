2fa429d4157991eca7b1b2eac7ce802ede4883a3
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-17 18:34:39 +0200

    Create Program.cs

---
+++ C#/Entity Framework Core - February 2021/01.ADO.NET/02. Villain Names/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _02._Villain_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server = .; Database = MinionsDB; Integrated Security = True;";
+            SqlConnection dbCon = new SqlConnection(connectionString);
+
+            dbCon.Open();
+
+            using (dbCon)
+            {
+                var query = new SqlCommand(" SELECT v.[Name], COUNT(mv.MinionId) AS MinionsCount" +
+                                           " FROM Villains v" +
+                                           " JOIN MinionsVillains mv ON mv.VillainId = v.Id" +
+                                           " GROUP BY v.[Name]" +
+                                           " HAVING COUNT(mv.MinionId) > 3" +
+                                           " ORDER BY MinionsCount", dbCon);
+
+                using (var reader = query.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string villainName = reader["Name"] as string;
+                        int? minionsCount = reader["MinionsCount"] as int?;
+
+                        Console.WriteLine($"{villainName} - {minionsCount}");
+                    }
+                }
+
+
+            }
+        }
+    }
+}
