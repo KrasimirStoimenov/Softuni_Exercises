28660acfb66f61c8595b885a6d98ffe5b5006634
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-19 19:13:56 +0200

    Create StartUp.cs

---
+++ C#/Entity Framework Core - February 2021/01.ADO.NET/07. Print All Minion Names/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _07._Print_All_Minion_Names
+{
+    class StartUp
+    {
+        private const string connectionString = "Server = .;Database = MinionsDB; Integrated Security = true;";
+        static void Main(string[] args)
+        {
+            using var dbCon = new SqlConnection(connectionString);
+            dbCon.Open();
+
+            string getMinionsQuery = "SELECT Name FROM Minions";
+
+            using var command = new SqlCommand(getMinionsQuery, dbCon);
+            using var reader = command.ExecuteReader();
+            List<string> minions = new List<string>();
+
+            while (reader.Read())
+            {
+                minions.Add(reader["Name"].ToString());
+            }
+
+            for (int i = 0; i < minions.Count/2; i++)
+            {
+                Console.WriteLine(minions[i]);
+                Console.WriteLine(minions[minions.Count-i-1]);
+            }
+
+            if(minions.Count %2 != 0)
+            {
+                Console.WriteLine(minions[minions.Count/2]);
+            }
+        }
+    }
+}
