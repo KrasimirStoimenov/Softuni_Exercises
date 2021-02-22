4e2ad667f6784a583d1f6dec68a4a1343a986043
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-22 18:50:36 +0200

    Create DbContext.cs

---
+++ C#/Entity Framework Core - February 2021/02. ORM-Fundamentals-MiniORM/MiniORM/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MiniORM
+{
+	public abstract class DbContext
+    {
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+
+        };
+    }
+	// TODO: Create your DbContext class here.
+}
