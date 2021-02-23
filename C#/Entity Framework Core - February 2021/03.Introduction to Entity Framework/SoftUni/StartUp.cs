ffb72c6d32341750e68d4493f355b854dbdb670a
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-23 21:35:15 +0200

    Added some tasks completed

---
+++ C#/Entity Framework Core - February 2021/03.Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //1.Import the SoftUni Database - Done
+            //2.Database First - Done
+
+            var db = new SoftUniContext();
+
+            //3.Employees Full Information
+                Console.WriteLine(GetEmployeesFullInformation(db));
+
+            //4.Employees with Salary Over 50 000
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.MiddleName,
+                    x.LastName,
+                    x.JobTitle,
+                    x.Salary,
+                })
+                .OrderBy(e => e.EmployeeId)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
