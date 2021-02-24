449b15c91354d380ea6295b38e67efed4380270f
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-24 21:20:34 +0200

    Update StartUp.cs

--- C#/Entity Framework Core - February 2021/03.Introduction to Entity Framework/SoftUni/StartUp.cs
+++ C#/Entity Framework Core - February 2021/03.Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -1,4 +1,6 @@
-﻿using SoftUni.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
 using System;
 using System.Linq;
 using System.Text;
@@ -15,16 +17,104 @@ namespace SoftUni
             var db = new SoftUniContext();
 
             //3.Employees Full Information
-            Console.WriteLine(new string('-',20));
+            Console.WriteLine(new string('-', 20));
             Console.WriteLine(GetEmployeesFullInformation(db));
 
             //4.Employees with Salary Over 50 000
-            Console.WriteLine(new string('-',20));
+            Console.WriteLine(new string('-', 20));
             Console.WriteLine(GetEmployeesWithSalaryOver50000(db));
 
             //5.Employees from Research and Development
-            Console.WriteLine(new string('-',20));
+            Console.WriteLine(new string('-', 20));
             Console.WriteLine(GetEmployeesFromResearchAndDevelopment(db));
+
+            //6.Adding a New Address and Updating Employee
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine(AddNewAddressToEmployee(db));
+
+            //7.Employees and Projects
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine(GetEmployeesInPeriod(db));
+        }
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(x => x.EmployeesProjects.Any(x => x.Project.StartDate.Year >= 2001 && x.Project.StartDate.Year <= 2003))
+                .Select(e => new
+                {
+                    EmployeeFirstName = e.FirstName,
+                    EmployeeLastName = e.LastName,
+                    ManagerFirstName = e.Manager.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    Projects = e.EmployeesProjects.Select(p => new
+                    {
+                        ProjectName = p.Project.Name,
+                        ProjectStartDate = p.Project.StartDate,
+                        ProjectEndDate = p.Project.EndDate,
+                    }),
+                })
+                .Take(10)
+                .ToList();
+
+            var sb = new StringBuilder();
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.EmployeeFirstName} {emp.EmployeeLastName} - Manager: {emp.ManagerFirstName} {emp.ManagerLastName}");
+
+                foreach (var project in emp.Projects)
+                {
+                    var projectStartDate = string.Format($"{project.ProjectStartDate}", "M/d/yyyy h:mm:ss tt");
+                    var projectEndDate = string.Empty;
+
+                    if (project.ProjectEndDate == null)
+                    {
+                        projectEndDate = "not finished";
+                    }
+                    else
+                    {
+                        projectEndDate = string.Format($"{project.ProjectEndDate}", "M/d/yyyy h:mm:ss tt");
+                    }
+
+                    sb.AppendLine($"--{project.ProjectName} - {projectStartDate} - {projectEndDate}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            var newAddress = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4,
+            };
+            context.Addresses.Add(newAddress);
+
+            var employeeNakov = context.Employees
+                .FirstOrDefault(x => x.LastName == "Nakov");
+
+            employeeNakov.Address = newAddress;
+
+            context.SaveChanges();
+
+            var addresses = context.Addresses
+                .Select(x => new
+                {
+                    x.AddressId,
+                    x.AddressText,
+                })
+                .OrderByDescending(x => x.AddressId)
+                .Take(10)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var address in addresses)
+            {
+                sb.AppendLine(address.AddressText);
+            }
+
+            return sb.ToString().TrimEnd();
 
 
         }
