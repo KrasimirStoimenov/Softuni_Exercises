3ddd07584135b627db568e78d964324c99a86265
Krasimir Stoimenov <Krassimir.Stoimenov@gmail.com>
2021-02-26 19:46:07 +0200

    02. ORM Fundamentals - Mini ORM

---
+++ C#/Entity Framework Core - February 2021/02.ORM Fundamentals/02. ORM-Fundamentals-MiniORM-Lab-Skeleton/MiniORM.App/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Server=.; Database=MiniORM; Integrated Security = true;";
+
+            var db = new SoftUniDbContext(connectionString);
+
+            db.Employees.Add(new Employee
+            {
+                FirstName = "xXx",
+                LastName = "Inserted",
+                DepartmentId = db.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = db.Employees.Last();
+
+            employee.FirstName = "Modified";
+
+            db.SaveChanges();
+        }
+    }
+}
