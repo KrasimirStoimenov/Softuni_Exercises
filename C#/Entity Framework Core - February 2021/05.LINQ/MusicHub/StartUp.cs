e9db3ba2f88eaba6e9f78ea187f78c391952490e
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-04 11:56:58 +0200

    Update StartUp.cs

--- C#/Entity Framework Core - February 2021/05.LINQ/MusicHub/StartUp.cs
+++ C#/Entity Framework Core - February 2021/05.LINQ/MusicHub/StartUp.cs
@@ -1,7 +1,8 @@
 ﻿namespace MusicHub
 {
     using System;
-
+    using System.Linq;
+    using System.Text;
     using Data;
     using Initializer;
 
@@ -9,17 +10,60 @@
     {
         public static void Main(string[] args)
         {
-            MusicHubDbContext context = 
+            MusicHubDbContext context =
                 new MusicHubDbContext();
 
             DbInitializer.ResetDatabase(context);
 
-            //Test your solutions here
+            Console.WriteLine(ExportAlbumsInfo(context, 9));
         }
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            throw new NotImplementedException();
+            var producerAlbums = context.Albums
+                .Where(x => x.ProducerId == producerId)
+                .Select(a => new
+                {
+                    AlbumName = a.Name,
+                    AlbumReleaseDate = a.ReleaseDate,
+                    ProducerName = a.Producer.Name,
+                    AlbumTotalPrice = a.Songs.Sum(x=>x.Price),
+                    AlbumSongs = a.Songs.Select(s => new
+                    {
+                        SongName = s.Name,
+                        Price = s.Price,
+                        SongWriterName = s.Writer.Name
+                    })
+                    .OrderByDescending(s => s.SongName)
+                    .ThenBy(s => s.SongWriterName)
+                    .ToList()
+                })
+                .OrderByDescending(x => x.AlbumTotalPrice)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var album in producerAlbums)
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}")
+                    .AppendLine($"-ReleaseDate: {album.AlbumReleaseDate:MM/dd/yyyy}")
+                    .AppendLine($"-ProducerName: {album.ProducerName}")
+                    .AppendLine($"-Songs:");
+
+                var counter = 1;
+
+                foreach (var song in album.AlbumSongs)
+                {
+                    sb.AppendLine($"---#{counter++}")
+                        .AppendLine($"---SongName: {song.SongName}")
+                        .AppendLine($"---Price: {song.Price:F2}")
+                        .AppendLine($"---Writer: {song.SongWriterName}");
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.AlbumTotalPrice:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
