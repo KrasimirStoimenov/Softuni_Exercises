5f52bf9f02efc8967d0b1d5ace98f530b3d8923f
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-06 13:12:25 +0200

    03.Golden Books

--- C#/Entity Framework Core - February 2021/06.Advanced Querying/BookShop/StartUp.cs
+++ C#/Entity Framework Core - February 2021/06.Advanced Querying/BookShop/StartUp.cs
@@ -16,9 +16,35 @@
 
             //02. Age Restriction
             Console.WriteLine(GetBooksByAgeRestriction(db, "miNor"));
+
+            //03. Golden Books
+            Console.WriteLine(GetGoldenBooks(db));
+
+
         }
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            //03. Golden Books
+
+            var books = context.Books
+                .Where(book => book.EditionType == EditionType.Gold && book.Copies < 5000)
+                .Select(x => new
+                {
+                    Id = x.BookId,
+                    Title = x.Title
+                })
+                .OrderBy(x => x.Id)
+                .ToList();
+
+            var result = string.Join(Environment.NewLine, books.Select(t => t.Title));
+
+            return result;
+        }
+
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
+            //02.Age Restriction
+
             var ageRestriction = Enum.Parse<AgeRestriction>(command, true);
 
             var books = context.Books
@@ -30,7 +56,7 @@
                 .OrderBy(x => x.Title)
                 .ToList();
 
-            var result = string.Join(Environment.NewLine,books.Select(t=>t.Title));
+            var result = string.Join(Environment.NewLine, books.Select(t => t.Title));
 
             return result;
         }
