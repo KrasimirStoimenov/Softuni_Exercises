91e62c6a7dbd3fda296b3702419b07eb00edb457
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-10 20:08:55 +0200

    Implemented Items Controller

--- C#/Entity Framework Core - February 2021/07. Auto-Mapping-Objects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
+++ C#/Entity Framework Core - February 2021/07. Auto-Mapping-Objects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -3,6 +3,7 @@
     using AutoMapper;
     using FastFood.Core.ViewModels.Categories;
     using FastFood.Core.ViewModels.Employees;
+    using FastFood.Core.ViewModels.Items;
     using FastFood.Models;
     using ViewModels.Positions;
 
@@ -33,6 +34,16 @@
 
             this.CreateMap<Employee, EmployeesAllViewModel>()
                 .ForMember(x => x.Position, y => y.MapFrom(s => s.Position.Name));
+
+            //Item
+
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<CreateItemInputModel, Item>();
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name));
         }
     }
 }
