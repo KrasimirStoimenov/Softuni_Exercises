79e2d987551e8b44211ee94fe60c0c0b840e863a
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-13 11:40:28 +0200

    02.ImportUsers

--- C#/Entity Framework Core - February 2021/08. JSON Processing/P01.Product-Shop/ProductShop/StartUp.cs
+++ C#/Entity Framework Core - February 2021/08. JSON Processing/P01.Product-Shop/ProductShop/StartUp.cs
@@ -13,8 +13,20 @@ namespace ProductShop
         public static void Main(string[] args)
         {
             var db = new ProductShopContext();
-            db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
+            //Query 2. Import Users
+            var inputJson = File.ReadAllText("../../../Datasets/users.json");
+            Console.WriteLine(ImportUsers(db, inputJson));
+        }
+        //Query 2. Import Users
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
         }
     }
 }
