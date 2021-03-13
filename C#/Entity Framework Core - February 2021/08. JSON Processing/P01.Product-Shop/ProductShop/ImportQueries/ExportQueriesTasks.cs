8380a24a6b7f72b0e0281c799c1b138865864bf2
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-13 16:27:39 +0200

    Organize Files And Folders

---
+++ C#/Entity Framework Core - February 2021/08. JSON Processing/P01.Product-Shop/ProductShop/ImportQueries/ExportQueriesTasks.cs
@@ -0,0 +1,123 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ProductShop.Data;
+
+namespace ProductShop.ImportQueries
+{
+    public static class ExportQueriesTasks
+    {
+        //Query 1. Export Products in Range
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .Select(x => new
+                {
+                    name = x.Name,
+                    price = x.Price,
+                    seller = x.Seller.FirstName + " " + x.Seller.LastName
+                })
+                .OrderBy(x => x.price)
+                .ToList();
+
+            var resultJson = JsonConvert.SerializeObject(products, Formatting.Indented);
+
+            return resultJson;
+        }
+
+        //Query 2. Export Successfully Sold Products
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(x => x.ProductsSold.Any(y => y.Buyer != null))
+                .Select(x => new
+                {
+                    firstName = x.FirstName,
+                    lastName = x.LastName,
+                    soldProducts = x.ProductsSold
+                        .Where(b => b.Buyer != null)
+                        .Select(p => new
+                        {
+                            name = p.Name,
+                            price = p.Price,
+                            buyerFirstName = p.Buyer.FirstName,
+                            buyerLastName = p.Buyer.LastName
+                        })
+                        .ToList()
+                })
+                .OrderBy(x => x.lastName)
+                .ThenBy(x => x.firstName)
+                .ToList();
+
+            var resultJson = JsonConvert.SerializeObject(users, Formatting.Indented);
+
+            return resultJson;
+        }
+
+        //Query 3. Export Categories by Products Count
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categories = context.Categories
+                .Select(x => new
+                {
+                    category = x.Name,
+                    productsCount = x.CategoryProducts.Count,
+                    averagePrice = x.CategoryProducts.Average(pr => pr.Product.Price).ToString("F2"),
+                    totalRevenue = x.CategoryProducts.Sum(pr => pr.Product.Price).ToString("F2")
+                })
+                .OrderByDescending(x => x.productsCount)
+                .ToList();
+
+            var resultJson = JsonConvert.SerializeObject(categories, Formatting.Indented);
+
+            return resultJson;
+        }
+
+        //Query 4. Export Users and Products
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Include(x => x.ProductsSold)
+                .ToList()
+                .Where(u => u.ProductsSold.Any(b => b.BuyerId != null))
+                .Select(x => new
+                {
+                    firstName = x.FirstName,
+                    lastName = x.LastName,
+                    age = x.Age,
+                    soldProducts = new
+                    {
+                        count = x.ProductsSold
+                            .Where(b => b.BuyerId != null)
+                            .Count(),
+                        products = x.ProductsSold
+                            .Where(b => b.BuyerId != null)
+                            .Select(pr => new
+                            {
+                                name = pr.Name,
+                                price = pr.Price
+                            })
+                    }
+                })
+                .OrderByDescending(x => x.soldProducts.products.Count())
+                .ToList();
+
+            var resultObj = new
+            {
+                usersCount = users.Count(),
+                users = users
+            };
+
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+
+            };
+
+            var resultJson = JsonConvert.SerializeObject(resultObj, Formatting.Indented, settings);
+
+            return resultJson;
+        }
+    }
+}
