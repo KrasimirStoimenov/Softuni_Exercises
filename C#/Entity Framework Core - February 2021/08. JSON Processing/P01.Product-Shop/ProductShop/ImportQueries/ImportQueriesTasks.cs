0a009936c339dde70e4d1b68486a68b38398b5af
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-13 12:41:22 +0200

    Reorganize Folders

---
+++ C#/Entity Framework Core - February 2021/08. JSON Processing/P01.Product-Shop/ProductShop/ImportQueries/ImportQueriesTasks.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop.ImportQueries
+{
+    public static class ImportQueriesTasks
+    {
+        //Query 2. Import Users
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+        //Query 3. Import Products
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+        //Query 4. Import Categories
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<Category[]>(inputJson)
+                .Where(x => x.Name != null)
+                .ToArray();
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Length}";
+        }
+
+
+        //Query 5. Import Categories and Products
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoryProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
+
+            context.CategoryProducts.AddRange(categoryProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoryProducts.Length}";
+        }
+    }
+}
--- C#/Entity Framework Core - February 2021/08. JSON Processing/P01.Product-Shop/ProductShop/StartUp.cs
+++ C#/Entity Framework Core - February 2021/08. JSON Processing/P01.Product-Shop/ProductShop/StartUp.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
 using ProductShop.Data;
-using ProductShop.Models;
+using ProductShop.ImportQueries;
 
 namespace ProductShop
 {
@@ -13,68 +10,30 @@ namespace ProductShop
         public static void Main(string[] args)
         {
             var db = new ProductShopContext();
+            db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
 
+            ImportQueriesTask(db);
+
+        }
+        public static void ImportQueriesTask(ProductShopContext db)
+        {
             //Query 2. Import Users
             var inputJson2 = File.ReadAllText("../../../Datasets/users.json");
-            Console.WriteLine(ImportUsers(db, inputJson2));
+            Console.WriteLine(ImportQueriesTasks.ImportUsers(db, inputJson2));
 
             //Query 3. Import Products
             var inputJson3 = File.ReadAllText("../../../Datasets/products.json");
-            Console.WriteLine(ImportProducts(db, inputJson3));
+            Console.WriteLine(ImportQueriesTasks.ImportProducts(db, inputJson3));
 
             //Query 4. Import Categories
             var inputJson4 = File.ReadAllText("../../../Datasets/categories.json");
-            Console.WriteLine(ImportCategories(db, inputJson4));
+            Console.WriteLine(ImportQueriesTasks.ImportCategories(db, inputJson4));
 
             //Query 5. Import Categories and Products
             var inputJson5 = File.ReadAllText("../../../Datasets/categories-products.json");
-            Console.WriteLine(ImportCategoryProducts(db, inputJson5));
-        }
-        //Query 5. Import Categories and Products
-        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
-        {
-            var categoryProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
-
-            context.CategoryProducts.AddRange(categoryProducts);
-            context.SaveChanges();
-
-            return $"Successfully imported {categoryProducts.Length}";
+            Console.WriteLine(ImportQueriesTasks.ImportCategoryProducts(db, inputJson5));
         }
 
-        //Query 4. Import Categories
-        public static string ImportCategories(ProductShopContext context, string inputJson)
-        {
-            var categories = JsonConvert.DeserializeObject<Category[]>(inputJson)
-                .Where(x => x.Name != null)
-                .ToArray();
-
-            context.Categories.AddRange(categories);
-            context.SaveChanges();
-
-            return $"Successfully imported {categories.Length}";
-        }
-
-        //Query 3. Import Products
-        public static string ImportProducts(ProductShopContext context, string inputJson)
-        {
-            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
-
-            context.Products.AddRange(products);
-            context.SaveChanges();
-
-            return $"Successfully imported {products.Length}";
-        }
-
-        //Query 2. Import Users
-        public static string ImportUsers(ProductShopContext context, string inputJson)
-        {
-            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
-
-            context.Users.AddRange(users);
-            context.SaveChanges();
-
-            return $"Successfully imported {users.Length}";
-        }
     }
 }
