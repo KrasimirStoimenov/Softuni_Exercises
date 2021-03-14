beebcd4324b5b52b0542d316220484b08fcc0ccc
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-14 12:18:54 +0200

    01. Create database Car-Dealer

---
+++ C#/Entity Framework Core - February 2021/08. JSON Processing/P02.Car-Dealer/CarDealer/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        private static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            
+        }
+        private void InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            });
+
+            mapper = new Mapper(config);
+        }
+    }
+}
