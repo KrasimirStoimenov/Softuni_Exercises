44ef732768b806ac50dc039f68ec85363e11f2a8
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-16 10:14:13 +0200

    Organize Folders

---
+++ C#/Entity Framework Core - February 2021/08. JSON Processing/P02.Car-Dealer/CarDealer/ImportExportData/ExportData.cs
@@ -0,0 +1,150 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO.OutputModels;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarDealer.ImportExportData
+{
+    public static class ExportData
+    {
+        private static IMapper mapper;
+
+        //Query 01. Export Ordered Customers
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            InitializeAutoMapper();
+
+            var customers = context.Customers
+                .OrderBy(x => x.BirthDate)
+                .ThenBy(x => x.IsYoungDriver)
+                .ToList();
+
+            var customersDto = mapper.Map<IEnumerable<CustomerOutputModel>>(customers);
+
+            var json = JsonConvert.SerializeObject(customersDto, Formatting.Indented);
+
+            return json;
+        }
+
+        //Query 02. Export Cars from Make Toyota
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            InitializeAutoMapper();
+
+            var toyotaCars = context.Cars
+                .Where(x => x.Make == "Toyota")
+                .OrderBy(x => x.Model)
+                .ThenByDescending(x => x.TravelledDistance)
+                .ToList();
+
+            var carsDto = mapper.Map<IEnumerable<CarOutputModel>>(toyotaCars);
+
+            var json = JsonConvert.SerializeObject(carsDto, Formatting.Indented);
+
+            return json;
+        }
+
+        //Query 03. Export Local Suppliers
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            InitializeAutoMapper();
+
+            var suppliers = context.Suppliers
+                .Include(x => x.Parts)
+                .Where(x => x.IsImporter == false)
+                .ToList();
+
+            var suppliersDto = mapper.Map<IEnumerable<SupplierOutputModel>>(suppliers);
+
+            var json = JsonConvert.SerializeObject(suppliersDto, Formatting.Indented);
+
+            return json;
+        }
+
+        //Query 04. Export Cars with Their List of Parts
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Select(x => new
+                {
+                    car = new
+                    {
+                        Make = x.Make,
+                        Model = x.Model,
+                        TravelledDistance = x.TravelledDistance,
+                    },
+                    parts = x.PartCars.Select(p => new
+                    {
+                        Name = p.Part.Name,
+                        Price = p.Part.Price.ToString("F2")
+                    })
+
+                })
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(cars, Formatting.Indented);
+
+            return json;
+        }
+
+        //Query 05. Export Total Sales by Customer
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .Where(x => x.Sales.Count >= 1)
+                .Select(x => new
+                {
+                    fullName = x.Name,
+                    boughtCars = x.Sales.Count,
+                    spentMoney = x.Sales.Select(c => c.Car.PartCars.Select(p => p.Part.Price).Sum()).Sum()
+                })
+                .OrderByDescending(x => x.spentMoney)
+                .ThenByDescending(x => x.boughtCars)
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(customers, Formatting.Indented);
+
+            return json;
+        }
+
+
+        //Query 06. Export Sales with Applied Discount
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            var sales = context.Sales
+                .Take(10)
+                .Select(x => new
+                {
+                    car = new
+                    {
+                        Make = x.Car.Make,
+                        Model = x.Car.Model,
+                        TravelledDistance = x.Car.TravelledDistance
+                    },
+                    customerName = x.Customer.Name,
+                    Discount = x.Discount.ToString("F2"),
+                    price = x.Car.PartCars.Select(p => p.Part.Price).Sum().ToString("F2"),
+                    priceWithDiscount = (x.Car.PartCars.Select(p => p.Part.Price).Sum() * (1 - (x.Discount * 0.01M))).ToString("F2")
+                })
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(sales, Formatting.Indented);
+
+            return json;
+        }
+
+        private static void InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            });
+
+            mapper = new Mapper(config);
+        }
+
+    }
+}
--- C#/Entity Framework Core - February 2021/08. JSON Processing/P02.Car-Dealer/CarDealer/StartUp.cs
+++ C#/Entity Framework Core - February 2021/08. JSON Processing/P02.Car-Dealer/CarDealer/StartUp.cs
@@ -1,168 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using AutoMapper;
 using CarDealer.Data;
-using CarDealer.DTO.OutputModels;
 using CarDealer.ImportExportData;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 
 namespace CarDealer
 {
     public class StartUp
     {
-        private static IMapper mapper;
         public static void Main(string[] args)
         {
             var db = new CarDealerContext();
             db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
-            
-            ImportDataQueries(db);
 
+            ImportDataQueries(db);
+            ExportDataQueries(db);
+        }
+
+        private static void ExportDataQueries(CarDealerContext db)
+        {
             //Query 01. Export Ordered Customers
-            Console.WriteLine(GetOrderedCustomers(db));
+            Console.WriteLine(ExportData.GetOrderedCustomers(db));
 
             //Query 02. Export Cars from Make Toyota
-            Console.WriteLine(GetCarsFromMakeToyota(db));
+            Console.WriteLine(ExportData.GetCarsFromMakeToyota(db));
 
             //Query 03. Export Local Suppliers
-            Console.WriteLine(GetLocalSuppliers(db));
+            Console.WriteLine(ExportData.GetLocalSuppliers(db));
 
             //Query 04. Export Cars with Their List of Parts
-            Console.WriteLine(GetCarsWithTheirListOfParts(db));
+            Console.WriteLine(ExportData.GetCarsWithTheirListOfParts(db));
 
             //Query 05. Export Total Sales by Customer
-            Console.WriteLine(GetTotalSalesByCustomer(db));
+            Console.WriteLine(ExportData.GetTotalSalesByCustomer(db));
 
             //Query 06. Export Sales with Applied Discount
-            Console.WriteLine(GetSalesWithAppliedDiscount(db));
-
-        }
-        //Query 06. Export Sales with Applied Discount
-        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
-        {
-            var sales = context.Sales
-                .Take(10)
-                .Select(x => new
-                {
-                    car = new
-                    {
-                        Make = x.Car.Make,
-                        Model = x.Car.Model,
-                        TravelledDistance = x.Car.TravelledDistance
-                    },
-                    customerName = x.Customer.Name,
-                    Discount = x.Discount.ToString("F2"),
-                    price = x.Car.PartCars.Select(p => p.Part.Price).Sum().ToString("F2"),
-                    priceWithDiscount = (x.Car.PartCars.Select(p => p.Part.Price).Sum() * (1 - (x.Discount * 0.01M))).ToString("F2")
-                })
-                .ToList();
-
-            var json = JsonConvert.SerializeObject(sales, Formatting.Indented);
-
-            return json;
-        }
-
-        //Query 05. Export Total Sales by Customer
-        public static string GetTotalSalesByCustomer(CarDealerContext context)
-        {
-            var customers = context.Customers
-                .Where(x => x.Sales.Count >= 1)
-                .Select(x => new
-                {
-                    fullName = x.Name,
-                    boughtCars = x.Sales.Count,
-                    spentMoney = x.Sales.Select(c => c.Car.PartCars.Select(p => p.Part.Price).Sum()).Sum()
-                })
-                .OrderByDescending(x => x.spentMoney)
-                .ThenByDescending(x => x.boughtCars)
-                .ToList();
-
-            var json = JsonConvert.SerializeObject(customers, Formatting.Indented);
-
-            return json;
-        }
-
-        //Query 04. Export Cars with Their List of Parts
-        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
-        {
-            var cars = context.Cars
-                .Select(x => new
-                {
-                    car = new
-                    {
-                        Make = x.Make,
-                        Model = x.Model,
-                        TravelledDistance = x.TravelledDistance,
-                    },
-                    parts = x.PartCars.Select(p => new
-                    {
-                        Name = p.Part.Name,
-                        Price = p.Part.Price.ToString("F2")
-                    })
-
-                })
-                .ToList();
-
-            var json = JsonConvert.SerializeObject(cars, Formatting.Indented);
-
-            return json;
-        }
-
-        //Query 03. Export Local Suppliers
-        public static string GetLocalSuppliers(CarDealerContext context)
-        {
-            InitializeAutoMapper();
-
-            var suppliers = context.Suppliers
-                .Include(x => x.Parts)
-                .Where(x => x.IsImporter == false)
-                .ToList();
-
-            var suppliersDto = mapper.Map<IEnumerable<SupplierOutputModel>>(suppliers);
-
-            var json = JsonConvert.SerializeObject(suppliersDto, Formatting.Indented);
-
-            return json;
-        }
-
-        //Query 02. Export Cars from Make Toyota
-        public static string GetCarsFromMakeToyota(CarDealerContext context)
-        {
-            InitializeAutoMapper();
-
-            var toyotaCars = context.Cars
-                .Where(x => x.Make == "Toyota")
-                .OrderBy(x => x.Model)
-                .ThenByDescending(x => x.TravelledDistance)
-                .ToList();
-
-            var carsDto = mapper.Map<IEnumerable<CarOutputModel>>(toyotaCars);
-
-            var json = JsonConvert.SerializeObject(carsDto, Formatting.Indented);
-
-            return json;
-        }
-
-        //Query 01. Export Ordered Customers
-        public static string GetOrderedCustomers(CarDealerContext context)
-        {
-            InitializeAutoMapper();
-
-            var customers = context.Customers
-                .OrderBy(x => x.BirthDate)
-                .ThenBy(x => x.IsYoungDriver)
-                .ToList();
-
-            var customersDto = mapper.Map<IEnumerable<CustomerOutputModel>>(customers);
-
-            var json = JsonConvert.SerializeObject(customersDto, Formatting.Indented);
-
-            return json;
+            Console.WriteLine(ExportData.GetSalesWithAppliedDiscount(db));
         }
 
         private static void ImportDataQueries(CarDealerContext db)
@@ -186,15 +59,6 @@ namespace CarDealer
             //Query 05. Import Sales
             var salesJson = File.ReadAllText("../../../Datasets/sales.json");
             Console.WriteLine(ImportData.ImportSales(db, salesJson));
-        }
-        private static void InitializeAutoMapper()
-        {
-            var config = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile<CarDealerProfile>();
-            });
-
-            mapper = new Mapper(config);
         }
 
     }
