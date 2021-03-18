e0b9f53626249b6e7070a901c1fc69370021840c
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-18 08:40:13 +0200

    Organize Folders

---
+++ C#/Entity Framework Core - February 2021/09. XML Processing/P01.Product-Shop/ProductShop/ImportExportDataQueries/ExportDataQueries.cs
@@ -0,0 +1,157 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using ProductShop.Data;
+using ProductShop.Dtos.Export;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.ImportExportDataQueries
+{
+    public class ExportDataQueries
+    {
+        private static IMapper mapper;
+
+        //Query 5. Products In Range
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            InitializeAutoMapper();
+
+            var products = context.Products
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .OrderBy(x => x.Price)
+                .Take(10)
+                .ProjectTo<ExportProductDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            var serializer = new XmlSerializer(typeof(ExportProductDto[]), new XmlRootAttribute("Products"));
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var sb = new StringBuilder();
+            using (var writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, products, namespaces);
+            };
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Query 6. Sold Products
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(x => x.ProductsSold.Count >= 1)
+                .Select(x => new ExportUserDto
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    SoldProducts = x.ProductsSold.Select(p => new UsersProductSoldDto
+                    {
+                        Name = p.Name,
+                        Price = p.Price
+                    })
+                    .ToArray()
+                })
+                .OrderBy(x => x.LastName)
+                .ThenBy(x => x.FirstName)
+                .Take(5)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportUserDto[]), new XmlRootAttribute("Users"));
+            xmlSerializer.Serialize(new StringWriter(sb), users, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Query 7. Categories By Products Count
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categories = context.Categories
+                .Select(x => new ExportCategoryDto
+                {
+                    Name = x.Name,
+                    Count = x.CategoryProducts.Count,
+                    AveragePrice = x.CategoryProducts.Average(p => p.Product.Price),
+                    TotalRevenue = x.CategoryProducts.Select(p => p.Product.Price).Sum()
+                })
+                .OrderByDescending(x => x.Count)
+                .ThenBy(x => x.TotalRevenue)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportCategoryDto[]), new XmlRootAttribute("Categories"));
+            xmlSerializer.Serialize(new StringWriter(sb), categories, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Query 8. Users and Products
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Include(x => x.ProductsSold)
+                .Where(u => u.ProductsSold.Count >= 1)
+                .ToArray()
+                .Select(x => new Q8UsersDto
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Age = x.Age,
+                    ProductsSoldByUser = new Q8ProductsSoldByUser
+                    {
+                        Count = x.ProductsSold.Count,
+                        Products = x.ProductsSold.Select(p => new Q8ProductsDto
+                        {
+                            Name = p.Name,
+                            Price = p.Price
+                        })
+                        .OrderByDescending(p => p.Price)
+                        .ToArray()
+                    }
+                })
+                .OrderByDescending(x => x.ProductsSoldByUser.Count)
+                .Take(10)
+                .ToArray();
+
+            var usersProducts = new Q8UsersProductsDto
+            {
+                Users = users,
+                Count = context.Users.Where(x => x.ProductsSold.Count >= 1).Count()
+            };
+
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var xmlSerializer = new XmlSerializer(typeof(Q8UsersProductsDto), new XmlRootAttribute("Users"));
+            xmlSerializer.Serialize(new StringWriter(sb), usersProducts, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static void InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = new Mapper(config);
+        }
+    }
+}
--- C#/Entity Framework Core - February 2021/09. XML Processing/P01.Product-Shop/ProductShop/StartUp.cs
+++ C#/Entity Framework Core - February 2021/09. XML Processing/P01.Product-Shop/ProductShop/StartUp.cs
@@ -1,21 +1,12 @@
-﻿using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using Microsoft.EntityFrameworkCore;
-using ProductShop.Data;
-using ProductShop.Dtos.Export;
+﻿using ProductShop.Data;
 using ProductShop.ImportExportDataQueries;
-using ProductShop.Models;
 using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
 
 namespace ProductShop
 {
     public class StartUp
     {
-        private static IMapper mapper;
         public static void Main(string[] args)
         {
             var db = new ProductShopContext();
@@ -23,147 +14,23 @@ namespace ProductShop
             db.Database.EnsureCreated();
 
             ImportData(db);
+            ExportData(db);
+        }
 
+        private static void ExportData(ProductShopContext db)
+        {
             //Query 5. Products In Range
-            Console.WriteLine(GetProductsInRange(db));
+            Console.WriteLine(ExportDataQueries.GetProductsInRange(db));
 
             //Query 6. Sold Products
-            Console.WriteLine(GetSoldProducts(db));
+            Console.WriteLine(ExportDataQueries.GetSoldProducts(db));
 
             //Query 7. Categories By Products Count
-            Console.WriteLine(GetCategoriesByProductsCount(db));
+            Console.WriteLine(ExportDataQueries.GetCategoriesByProductsCount(db));
 
             //Query 8. Users and Products
-            Console.WriteLine(GetUsersWithProducts(db));
+            Console.WriteLine(ExportDataQueries.GetUsersWithProducts(db));
 
-        }
-        //Query 8. Users and Products
-        public static string GetUsersWithProducts(ProductShopContext context)
-        {
-            var users = context.Users
-                .Include(x => x.ProductsSold)
-                .Where(u => u.ProductsSold.Count >= 1)
-                .ToArray()
-                .Select(x => new Q8UsersDto
-                {
-                    FirstName = x.FirstName,
-                    LastName = x.LastName,
-                    Age = x.Age,
-                    ProductsSoldByUser = new Q8ProductsSoldByUser
-                    {
-                        Count = x.ProductsSold.Count,
-                        Products = x.ProductsSold.Select(p => new Q8ProductsDto
-                        {
-                            Name = p.Name,
-                            Price = p.Price
-                        })
-                        .OrderByDescending(p => p.Price)
-                        .ToArray()
-                    }
-                })
-                .OrderByDescending(x => x.ProductsSoldByUser.Count)
-                .Take(10)
-                .ToArray();
-
-            var usersProducts = new Q8UsersProductsDto
-            {
-                Users = users,
-                Count = context.Users.Where(x=>x.ProductsSold.Count>=1).Count()
-            };
-
-
-            var sb = new StringBuilder();
-
-            var namespaces = new XmlSerializerNamespaces();
-            namespaces.Add(string.Empty, string.Empty);
-
-            var xmlSerializer = new XmlSerializer(typeof(Q8UsersProductsDto), new XmlRootAttribute("Users"));
-            xmlSerializer.Serialize(new StringWriter(sb), usersProducts, namespaces);
-
-            return sb.ToString().TrimEnd();
-        }
-        //Query 7. Categories By Products Count
-        public static string GetCategoriesByProductsCount(ProductShopContext context)
-        {
-            var categories = context.Categories
-                .Select(x => new ExportCategoryDto
-                {
-                    Name = x.Name,
-                    Count = x.CategoryProducts.Count,
-                    AveragePrice = x.CategoryProducts.Average(p => p.Product.Price),
-                    TotalRevenue = x.CategoryProducts.Select(p => p.Product.Price).Sum()
-                })
-                .OrderByDescending(x => x.Count)
-                .ThenBy(x => x.TotalRevenue)
-                .ToArray();
-
-            var sb = new StringBuilder();
-
-            var namespaces = new XmlSerializerNamespaces();
-            namespaces.Add(string.Empty, string.Empty);
-
-            var xmlSerializer = new XmlSerializer(typeof(ExportCategoryDto[]), new XmlRootAttribute("Categories"));
-            xmlSerializer.Serialize(new StringWriter(sb), categories, namespaces);
-
-            return sb.ToString().TrimEnd();
-        }
-
-        //Query 6. Sold Products
-        public static string GetSoldProducts(ProductShopContext context)
-        {
-            var users = context.Users
-                .Where(x => x.ProductsSold.Count >= 1)
-                .Select(x => new ExportUserDto
-                {
-                    FirstName = x.FirstName,
-                    LastName = x.LastName,
-                    SoldProducts = x.ProductsSold.Select(p => new UsersProductSoldDto
-                    {
-                        Name = p.Name,
-                        Price = p.Price
-                    })
-                    .ToArray()
-                })
-                .OrderBy(x => x.LastName)
-                .ThenBy(x => x.FirstName)
-                .Take(5)
-                .ToArray();
-
-            var sb = new StringBuilder();
-
-            var namespaces = new XmlSerializerNamespaces();
-            namespaces.Add(string.Empty, string.Empty);
-
-            var xmlSerializer = new XmlSerializer(typeof(ExportUserDto[]), new XmlRootAttribute("Users"));
-            xmlSerializer.Serialize(new StringWriter(sb), users, namespaces);
-
-            return sb.ToString().TrimEnd();
-        }
-
-        //Query 5. Products In Range
-        public static string GetProductsInRange(ProductShopContext context)
-        {
-            InitializeAutoMapper();
-
-            var products = context.Products
-                .Where(x => x.Price >= 500 && x.Price <= 1000)
-                .OrderBy(x => x.Price)
-                .Take(10)
-                .ProjectTo<ExportProductDto>(mapper.ConfigurationProvider)
-                .ToArray();
-
-            var serializer = new XmlSerializer(typeof(ExportProductDto[]), new XmlRootAttribute("Products"));
-
-            var namespaces = new XmlSerializerNamespaces();
-            namespaces.Add(string.Empty, string.Empty);
-
-            var sb = new StringBuilder();
-            using (var writer = new StringWriter(sb))
-            {
-                serializer.Serialize(writer, products, namespaces);
-            };
-
-            return sb.ToString().TrimEnd();
         }
 
         private static void ImportData(ProductShopContext db)
@@ -184,16 +51,5 @@ namespace ProductShop
             var categoriesProductsXml = File.ReadAllText("../../../Datasets/categories-products.xml");
             Console.WriteLine(ImportDataQueries.ImportCategoryProducts(db, categoriesProductsXml));
         }
-
-        private static void InitializeAutoMapper()
-        {
-            var config = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile<ProductShopProfile>();
-            });
-
-            mapper = new Mapper(config);
-        }
-
     }
 }
