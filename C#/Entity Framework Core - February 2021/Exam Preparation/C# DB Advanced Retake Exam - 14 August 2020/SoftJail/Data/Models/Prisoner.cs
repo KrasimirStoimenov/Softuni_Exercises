656ce3719ede60b99ffa35df94d663b31209f953
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-23 16:33:45 +0200

    P01. Model Definition

--- C#/Entity Framework Core - February 2021/Exam Preparation/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/Data/Models/Prisoner.cs
+++ C#/Entity Framework Core - February 2021/Exam Preparation/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/Data/Models/Prisoner.cs
@@ -1,7 +1,45 @@
-﻿namespace SoftJail.Data.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftJail.Data.Models
 {
     public class Prisoner
     {
+        public Prisoner()
+        {
+            this.Mails = new HashSet<Mail>();
+            this.PrisonerOfficers = new HashSet<OfficerPrisoner>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string FullName { get; set; }
+
+        [Required]
+        [RegularExpression(@"The [A-Z][a-z]+")]
+        public string Nickname { get; set; }
+
+        [Range(18, 65)]
+        public int Age { get; set; }
+
+        [Required]
+        public DateTime IncarcerationDate { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        [Range(0, Double.PositiveInfinity)]
+        public decimal? Bail { get; set; }
+
+        public int? CellId { get; set; }
+        public Cell Cell { get; set; }
+
+        public ICollection<Mail> Mails { get; set; }
+
+        public ICollection<OfficerPrisoner> PrisonerOfficers { get; set; }
 
     }
 }
--- C#/Entity Framework Core - February 2021/Exam Preparation/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/Data/SoftJailDbContext.cs
+++ C#/Entity Framework Core - February 2021/Exam Preparation/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/Data/SoftJailDbContext.cs
@@ -1,29 +1,49 @@
 ﻿namespace SoftJail.Data
 {
-	using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using SoftJail.Data.Models;
 
-	public class SoftJailDbContext : DbContext
-	{
-		public SoftJailDbContext()
-		{
-		}
+    public class SoftJailDbContext : DbContext
+    {
+        public SoftJailDbContext()
+        {
+        }
 
-		public SoftJailDbContext(DbContextOptions options)
-			: base(options)
-		{
-		}
+        public SoftJailDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			if (!optionsBuilder.IsConfigured)
-			{
-				optionsBuilder
-					.UseSqlServer(Configuration.ConnectionString);
-			}
-		}
+        public DbSet<Cell> Cells { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Mail> Mails { get; set; }
+        public DbSet<Officer> Officers { get; set; }
+        public DbSet<Prisoner> Prisoners { get; set; }
+        public DbSet<OfficerPrisoner> OfficersPrisoners { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
-		}
-	}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<OfficerPrisoner>(x =>
+            {
+
+                x.HasKey(c => new { c.OfficerId, c.PrisonerId });
+
+                x
+                    .HasOne(op => op.Prisoner)
+                    .WithMany(p => p.PrisonerOfficers)
+                    .HasForeignKey(op => op.PrisonerId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            });
+        }
+    }
 }
