5655af4421293f854072daf190a34be2faab3c06
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-29 14:52:03 +0300

    Code First Model Definition

---
+++ C#/Entity Framework Core - February 2021/Exam Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VaporStore.Data.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public ICollection<GameTag> GameTags { get; set; }
+
+    }
+}
+//•	Id – integer, Primary Key
+//•	Name – text (required)
+//•	GameTags - collection of type GameTag
+
