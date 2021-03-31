dc27d0a16cdbb4ab3c5358e58628e85e3f51302e
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-03-31 09:20:15 +0300

    Export Games By Genre

--- C#/Entity Framework Core - February 2021/Exam Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
+++ C#/Entity Framework Core - February 2021/Exam Preparation/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Serializer.cs
@@ -1,18 +1,46 @@
 ﻿namespace VaporStore.DataProcessor
 {
-	using System;
-	using Data;
+    using System;
+    using System.Linq;
+    using Data;
+    using Newtonsoft.Json;
 
-	public static class Serializer
-	{
-		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
-		{
-			throw new NotImplementedException();
-		}
+    public static class Serializer
+    {
+        public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
+        {
+            var games = context.Genres
+                .ToList()
+                .Where(x => genreNames.Contains(x.Name))
+                .Select(g => new
+                {
+                    Id = g.Id,
+                    Genre = g.Name,
+                    Games = g.Games.Select(x => new
+                    {
+                        Id = x.Id,
+                        Title = x.Name,
+                        Developer = x.Developer.Name,
+                        Tags = string.Join(", ", x.GameTags.Select(gt => gt.Tag.Name)),
+                        Players = x.Purchases.Count
+                    })
+                    .Where(x => x.Players > 0)
+                    .OrderByDescending(x => x.Players)
+                    .ThenBy(x => x.Id),
 
-		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
-		{
-			throw new NotImplementedException();
-		}
-	}
+                    TotalPlayers = g.Games.Sum(x => x.Purchases.Count())
+                })
+                .OrderByDescending(x => x.TotalPlayers)
+                .ThenBy(x => x.Id);
+
+            var serialized = JsonConvert.SerializeObject(games, Formatting.Indented);
+
+            return serialized.ToString();
+        }
+
+        public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
