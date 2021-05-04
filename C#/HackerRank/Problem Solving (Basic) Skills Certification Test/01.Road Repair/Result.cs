6354f4f252f1595db5f5262af98b7757f8b8cc90
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-05-04 21:42:40 +0300

    Hacker Rank Certifiation - 01.Road Repair

---
+++ C#/HackerRank/Problem Solving (Basic) Skills Certification Test/01.Road Repair/Result.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Road_Repair
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'getMinCost' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY crew_id
+         *  2. INTEGER_ARRAY job_id
+         */
+
+        public static long getMinCost(List<int> crew_id, List<int> job_id)
+        {
+            var minCost = 0;
+
+            var sortedCrew = crew_id.OrderBy(x => x).ToList();
+            var sortedJob = job_id.OrderBy(x => x).ToList();
+
+            for (int i = 0; i < sortedCrew.Count; i++)
+            {
+                var currentCrew = sortedCrew[i];
+                var currentJob = sortedJob[i];
+
+                if (currentJob >= currentCrew)
+                {
+                    minCost += currentJob - currentCrew;
+                }
+
+                else if (currentJob < currentCrew)
+
+                {
+                    minCost += currentCrew - currentJob;
+                }
+            }
+
+            if (minCost < 0)
+            {
+                minCost = 0;
+            }
+
+            return minCost;
+
+        }
+    }
+}
---
+++ C#/HackerRank/Problem Solving (Basic) Skills Certification Test/01.Road Repair/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01.Road_Repair
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int crew_idCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> crew_id = new List<int>();
+
+            for (int i = 0; i < crew_idCount; i++)
+            {
+                int crew_idItem = Convert.ToInt32(Console.ReadLine().Trim());
+                crew_id.Add(crew_idItem);
+            }
+
+            int job_idCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> job_id = new List<int>();
+
+            for (int i = 0; i < job_idCount; i++)
+            {
+                int job_idItem = Convert.ToInt32(Console.ReadLine().Trim());
+                job_id.Add(job_idItem);
+            }
+
+            long result = Result.getMinCost(crew_id, job_id);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
+
