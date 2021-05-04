0dfe97240dc98c0a3dfe3607eb02ef8327a538d5
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-05-04 21:42:56 +0300

    Hacker Rank Certifiaction - 02. Active Traders

---
+++ C#/HackerRank/Problem Solving (Basic) Skills Certification Test/02.Active Traders/Result.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Active_Traders
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'mostActive' function below.
+         *
+         * The function is expected to return a STRING_ARRAY.
+         * The function accepts STRING_ARRAY customers as parameter.
+         */
+
+        public static List<string> mostActive(List<string> customers)
+        {
+            var count = customers.Count;
+
+            var activeCustomers = new Dictionary<string, int>();
+
+            foreach (var customer in customers)
+            {
+                if (activeCustomers.ContainsKey(customer))
+                {
+                    activeCustomers[customer]++;
+                }
+                else
+                {
+                    activeCustomers.Add(customer, 1);
+                }
+            }
+
+            var mostActiveCustomers = new List<string>();
+
+            foreach (var customer in activeCustomers)
+            {
+                var customerContibute = customer.Value / (count * 1.0) * 100;
+
+                if (customerContibute >= 5)
+                {
+                    mostActiveCustomers.Add(customer.Key);
+                }
+            }
+
+            return mostActiveCustomers.OrderBy(x => x).ToList();
+        }
+    }
+}
---
+++ C#/HackerRank/Problem Solving (Basic) Skills Certification Test/02.Active Traders/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _02.Active_Traders
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int customersCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> customers = new List<string>();
+
+            for (int i = 0; i < customersCount; i++)
+            {
+                string customersItem = Console.ReadLine();
+                customers.Add(customersItem);
+            }
+
+            List<string> result = Result.mostActive(customers);
+
+            //textWriter.WriteLine(String.Join("\n", result));
+            //
+            //textWriter.Flush();
+            //textWriter.Close();
+
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+
+}
