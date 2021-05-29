7826a577c0a036667621a2ae487fa02f72628110
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-05-29 18:38:58 +0300

    Initial Created Server

---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/Handmade HTTP Server/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using HandmadeHttpServer.Server;
+
+namespace HandmadeHttpServer
+{
+    class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new HttpServer("127.0.0.1", 9090);
+
+            await server.Start();
+        }
+    }
+}
---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/HttpServer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandmadeHttpServer.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener listener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+            this.listener = new TcpListener(this.ipAddress, this.port);
+        }
+
+        public async Task Start()
+        {
+            listener.Start();
+
+            Console.WriteLine($"Server started at {this.ipAddress}:{this.port}");
+
+            while (true)
+            {
+                var connection = await this.listener.AcceptTcpClientAsync();
+                var stream = connection.GetStream();
+
+                var requestText = await this.ReadRequest(stream);
+
+                Console.WriteLine(requestText);
+
+                await WriteResponse(stream);
+
+                connection.Close();
+            }
+        }
+
+        private async Task<string> ReadRequest(NetworkStream stream)
+        {
+            var requestBuilder = new StringBuilder();
+
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
+
+            while (stream.DataAvailable)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                var request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                requestBuilder.Append(request);
+            }
+
+            return requestBuilder.ToString();
+        }
+
+        private async Task WriteResponse(NetworkStream stream)
+        {
+            var content = "Hello from My Web Server";
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+
+            var response = @$"   HTTP/1.1 200 OK
+                                Date:{DateTime.UtcNow}
+                                Server:My web server
+                                Content-Type: text/plain
+                                Content-Length: {contentLength}
+
+                                {content}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            await stream.WriteAsync(responseBytes);
+        }
+    }
+}
