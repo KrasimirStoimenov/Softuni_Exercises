fe2392267ed15f35f413ea02185e853b6c5923d8
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-01 14:38:09 +0300

    Implement HttpResponse Class

---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Http/HttpResponse/HttpResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace HandmadeHTTPServer.Server.Http.HttpResponse
+{
+    public class HttpResponse
+    {
+        private Dictionary<string, HttpHeader> headers;
+
+        public HttpResponse()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public string Content { get; private set; }
+    }
+}
---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Http/HttpResponse/HttpStatusCode.cs
@@ -0,0 +1,9 @@
+﻿namespace HandmadeHTTPServer.Server.Http.HttpResponse
+{
+    public enum HttpStatusCode
+    {
+        OK = 200,
+        NOTFOUND = 404
+
+    }
+}
