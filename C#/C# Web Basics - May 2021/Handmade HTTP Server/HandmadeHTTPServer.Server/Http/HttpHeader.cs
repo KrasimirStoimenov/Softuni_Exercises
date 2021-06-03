3d5309af82cb95c13c71082848d2481659c4c1b9
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-03 10:14:28 +0300

    Implement constructor for HTTP Header and added new guard class

--- C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Http/HttpHeader.cs
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Http/HttpHeader.cs
@@ -1,8 +1,23 @@
-﻿namespace HandmadeHTTPServer.Server.Http
+﻿using HandmadeHTTPServer.Server.Common;
+
+namespace HandmadeHTTPServer.Server.Http
 {
     public class HttpHeader
     {
-        public string Name { get; set; }
-        public string Value { get; set; }
+        public HttpHeader(string name, string value)
+        {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
+            this.Name = name;
+            this.Value = value;
+        }
+        public string Name { get; init; }
+        public string Value { get; init; }
+
+        public override string ToString()
+        {
+            return $"{this.Name}: {this.Value}";
+        }
     }
 }
