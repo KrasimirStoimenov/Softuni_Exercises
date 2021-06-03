25f50eb85009cef5ff8598ad46206339fb6d692f
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-03 09:50:06 +0300

    Update HttpResponse.cs

--- C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Http/HttpResponse/HttpResponse.cs
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Http/HttpResponse/HttpResponse.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace HandmadeHTTPServer.Server.Http.HttpResponse
 {
     public class HttpResponse
     {
-        private Dictionary<string, HttpHeader> headers;
-
         public HttpResponse()
         {
-            this.headers = new Dictionary<string, HttpHeader>();
+            this.Headers = new List<HttpHeader>();
         }
 
-        public HttpStatusCode StatusCode { get; private set; }
+        public HttpStatusCode StatusCode { get; init; }
 
-        public string Content { get; private set; }
+        public string Content { get; init; }
+
+        public List<HttpHeader> Headers { get; private set; }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"HTTP/1.1 {(int)this.StatusCode} {this.StatusCode}");
+
+            foreach (var header in this.Headers)
+            {
+                result.AppendLine(header.ToString());
+            }
+
+            if (!string.IsNullOrEmpty(this.Content))
+            {
+                result.AppendLine();
+
+                result.Append(this.Content);
+            }
+
+            return result.ToString();
+        }
     }
 }
