efc97cbac027dc7514b6c3c449f00b42ccceea98
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-05 16:59:12 +0300

    Implement abstract Controller class and Routing Table extensions

---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Controllers/Controller.cs
@@ -0,0 +1,31 @@
+﻿using HandmadeHTTPServer.Server.Http.HttpRequest;
+using HandmadeHTTPServer.Server.Http.HttpResponse;
+using HandmadeHTTPServer.Server.Responses;
+
+namespace HandmadeHTTPServer.Server.Controllers
+{
+    public abstract class Controller
+    {
+        protected Controller(HttpRequest request)
+        {
+            this.Request = request;
+        }
+
+        protected HttpRequest Request { get; init; }
+
+        protected HttpResponse Text(string text)
+        {
+            return new TextResponse(text);
+        }
+
+        protected HttpResponse Html(string html)
+        {
+            return new HtmlResponse(html);
+        }
+
+        protected HttpResponse Redirect(string location)
+        {
+            return new RedirectResponse(location);
+        }
+    }
+}
---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Controllers/RoutingTableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HandmadeHTTPServer.Server.Http.HttpRequest;
+using HandmadeHTTPServer.Server.Http.HttpResponse;
+using HandmadeHTTPServer.Server.Routing;
+
+namespace HandmadeHTTPServer.Server.Controllers
+{
+    public static class RoutingTableExtensions
+    {
+        public static IRoutingTable MapGet<TController>(
+            this IRoutingTable routingTable,
+            string path,
+            Func<TController, HttpResponse> controllerFunction)
+            where TController : Controller
+        {
+            return routingTable.MapGet(path, request => controllerFunction(CreateController<TController>(request)));
+        }
+
+        public static IRoutingTable MapPost<TController>(
+            this IRoutingTable routingTable,
+            string path,
+            Func<TController, HttpResponse> controllerFunction)
+            where TController : Controller
+        {
+            return routingTable.MapPost(path, request => controllerFunction(CreateController<TController>(request)));
+        }
+
+        private static TController CreateController<TController>(HttpRequest request)
+        {
+            return (TController)Activator.CreateInstance(typeof(TController), new[] { request });
+        }
+    }
+}
