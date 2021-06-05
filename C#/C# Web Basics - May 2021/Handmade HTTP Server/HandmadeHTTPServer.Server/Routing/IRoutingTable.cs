82d5b8cb8744f62d1dc1953a401b81458377a8d7
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-05 16:38:57 +0300

    Implemented Routing Table

---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Routing/IRoutingTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HandmadeHTTPServer.Server.Http.HttpRequest;
+using HandmadeHTTPServer.Server.Http.HttpResponse;
+
+namespace HandmadeHTTPServer.Server.Routing
+{
+    public interface IRoutingTable
+    {
+        IRoutingTable Map(HttpMethod method, string path, HttpResponse response);
+        IRoutingTable Map(HttpMethod method, string path, Func<HttpRequest, HttpResponse> responseFunction);
+        IRoutingTable MapGet(string path, HttpResponse response);
+        IRoutingTable MapGet(string path, Func<HttpRequest, HttpResponse> responseFunction);
+        IRoutingTable MapPost(string path, HttpResponse response);
+        IRoutingTable MapPost(string path, Func<HttpRequest, HttpResponse> responseFunction);
+    }
+}
---
+++ C#/C# Web Basics - May 2021/Handmade HTTP Server/HandmadeHTTPServer.Server/Routing/RoutingTable.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using HandmadeHTTPServer.Server.Common;
+using HandmadeHTTPServer.Server.Http.HttpRequest;
+using HandmadeHTTPServer.Server.Http.HttpResponse;
+using HandmadeHTTPServer.Server.Responses;
+
+namespace HandmadeHTTPServer.Server.Routing
+{
+    public class RoutingTable : IRoutingTable
+    {
+        private readonly Dictionary<HttpMethod, Dictionary<string, Func<HttpRequest, HttpResponse>>> routes;
+
+        public RoutingTable()
+        {
+            this.routes = new()
+            {
+                [HttpMethod.GET] = new(),
+                [HttpMethod.POST] = new(),
+                [HttpMethod.PUT] = new(),
+                [HttpMethod.DELETE] = new(),
+            };
+        }
+
+        public IRoutingTable Map(HttpMethod method, string path, HttpResponse response)
+        {
+            return this.Map(method, path, request => response);
+        }
+
+        public IRoutingTable Map(HttpMethod method, string path, Func<HttpRequest, HttpResponse> responseFunction)
+        {
+            Guard.AgainstNull(path, nameof(path));
+            Guard.AgainstNull(responseFunction, nameof(responseFunction));
+
+            this.routes[method][path.ToLower()] = responseFunction;
+
+            return this;
+        }
+
+        public IRoutingTable MapGet(string path, HttpResponse response)
+        {
+            return this.MapGet(path, request => response);
+        }
+
+        public IRoutingTable MapGet(string path, Func<HttpRequest, HttpResponse> responseFunction)
+        {
+            return this.Map(HttpMethod.GET, path, responseFunction);
+        }
+
+        public IRoutingTable MapPost(string path, HttpResponse response)
+        {
+            return this.MapPost(path, request => response);
+        }
+
+        public IRoutingTable MapPost(string path, Func<HttpRequest, HttpResponse> responseFunction)
+        {
+            return this.Map(HttpMethod.POST, path, responseFunction);
+        }
+
+        public HttpResponse ExecuteRequest(HttpRequest request)
+        {
+            var requestMethod = request.Method;
+            var requestPath = request.Path.ToLower();
+
+            if (!this.routes.ContainsKey(requestMethod) || !this.routes[requestMethod].ContainsKey(requestPath))
+            {
+                return new NotFoundResponse();
+            }
+
+            var responseFunction = this.routes[requestMethod][requestPath];
+
+            return responseFunction(request);
+        }
+    }
+}
