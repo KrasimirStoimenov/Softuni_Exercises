46256e9c4cf367eee98d56fcdc658d4033bd1b6a
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-19 12:45:19 +0300

    CarShop exam initial create

---
+++ C#/C# Web Basics - May 2021/ExamPreparation/CarShop/Apps/CarShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=CarShop;Integrated Security=true;");
+            }
+        }
+    }
+}
---
+++ C#/C# Web Basics - May 2021/ExamPreparation/CarShop/Apps/CarShop/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace CarShop
+{
+    using System.Threading.Tasks;
+    using CarShop.Data;
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers())
+                .WithServices(services => services
+                    .Add<IViewEngine, CompilationViewEngine>()
+                    .Add<ApplicationDbContext>())
+                .WithConfiguration<ApplicationDbContext>(context => context
+                    .Database.Migrate())
+                .Start();
+    }
+}
