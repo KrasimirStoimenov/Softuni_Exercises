a0a08c59b6285e1c4624df7f855723c7d8247776
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-21 18:14:50 +0300

    Git exam preparation initial create

---
+++ C#/C# Web Basics - May 2021/ExamPreparation/Git/Apps/Git/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Git.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=Git;Integrated Security=true;");
+            }
+        }
+    }
+}
---
+++ C#/C# Web Basics - May 2021/ExamPreparation/Git/Apps/Git/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace Git
+{
+    using Git.Data;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers())
+                .WithServices(services => services
+                    .Add<IViewEngine, CompilationViewEngine>()
+                    .Add<ApplicationDbContext>())
+                .WithConfiguration<ApplicationDbContext>(context => context
+                    .Database.Migrate())
+                .Start();
+    }
+}
