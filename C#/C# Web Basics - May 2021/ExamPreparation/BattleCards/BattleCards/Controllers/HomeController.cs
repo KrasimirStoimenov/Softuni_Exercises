1a04420614a607726875dc68d5797ce57ee0be1f
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-06-23 13:30:30 +0300

    BattleCards exam preparation initial create

---
+++ C#/C# Web Basics - May 2021/ExamPreparation/BattleCards/BattleCards/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using MyWebServer.Http;
+using MyWebServer.Controllers;
+
+namespace BattleCards.Controllers
+{
+    public class HomeController : Controller
+    { 
+        public HttpResponse Index()
+        {
+            return this.View();
+        }
+    }
+}
