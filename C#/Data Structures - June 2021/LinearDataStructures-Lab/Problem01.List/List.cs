9a655204ba2e732a9670e92edee45978e5baa66c
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-07-04 23:00:34 +0300

    Implemented list data structure

--- C#/Data Structures - June 2021/LinearDataStructures-Lab/Problem01.List/List.cs
+++ C#/Data Structures - June 2021/LinearDataStructures-Lab/Problem01.List/List.cs
@@ -7,26 +7,34 @@
     public class List<T> : IAbstractList<T>
     {
         private const int DEFAULT_CAPACITY = 4;
-        private T[] _items;
+        private T[] items;
 
         public List()
-            : this(DEFAULT_CAPACITY) {
+            : this(DEFAULT_CAPACITY)
+        {
         }
 
         public List(int capacity)
         {
-            throw new NotImplementedException();
+            if (capacity < 0)
+            {
+                throw new ArgumentException($"{nameof(capacity)} should be positive number!");
+            }
+
+            this.items = new T[capacity];
         }
 
         public T this[int index]
         {
             get
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                return this.items[index];
             }
             set
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                this.items[index] = value;
             }
         }
 
@@ -34,41 +42,127 @@
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count++] = item;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
-        }
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                var currentItem = this.items[i];
 
+                if (currentItem.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            var indexOfT = -1;
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                var currentItem = this.items[i];
+
+                if (currentItem.Equals(item))
+                {
+                    indexOfT = i;
+
+                    break;
+                }
+            }
+
+            return indexOfT;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            for (int i = this.Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+
+            this.items[index] = item;
+            this.Count++;
+
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                var currentItem = this.items[i];
+
+                if (currentItem.Equals(item))
+                {
+                    for (int j = i; j < this.Count-1; j++)
+                    {
+                        this.items[j] = this.items[j + 1];
+                    }
+
+                    this.items[this.Count--] = default;
+
+                    return true;
+                }
+
+            }
+
+            return false;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            for (int i = index; i < items.Length - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[this.Count--] = default;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.items[i];
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator() 
-            => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void Resize()
+        {
+            var newArray = new T[this.items.Length * 2];
+
+            Array.Copy(this.items, newArray, this.items.Length);
+
+            this.items = newArray;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
     }
 }
