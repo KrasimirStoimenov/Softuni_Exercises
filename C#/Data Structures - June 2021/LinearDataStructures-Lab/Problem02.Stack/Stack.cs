02b116349a68776a38adf287bf9c4493679ef0c4
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-07-05 19:05:10 +0300

    Implemented stack

--- C#/Data Structures - June 2021/LinearDataStructures-Lab/Problem02.Stack/Stack.cs
+++ C#/Data Structures - June 2021/LinearDataStructures-Lab/Problem02.Stack/Stack.cs
@@ -6,36 +6,77 @@
 
     public class Stack<T> : IAbstractStack<T>
     {
-        private Node<T> _top;
+        private Node<T> top;
 
         public int Count { get; private set; }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var topNode = this.top;
+
+            while (topNode != null)
+            {
+                var currentValue = topNode.Value;
+
+                if (currentValue.Equals(item))
+                {
+                    return true;
+                }
+
+                topNode = topNode.Next;
+            }
+
+            return false;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.ValidateIfThereIsElementInTheStack();
+
+            return this.top.Value;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            this.ValidateIfThereIsElementInTheStack();
+
+            var removedNode = this.top.Value;
+            this.top = this.top.Next;
+
+            this.Count--;
+
+            return removedNode;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>(item);
+            newNode.Next = this.top;
+
+            this.top = newNode;
+            this.Count++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var topNode = this.top;
+
+            while (topNode != null)
+            {
+                yield return topNode.Value;
+                topNode = topNode.Next;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator() 
-            => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void ValidateIfThereIsElementInTheStack()
+        {
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }
