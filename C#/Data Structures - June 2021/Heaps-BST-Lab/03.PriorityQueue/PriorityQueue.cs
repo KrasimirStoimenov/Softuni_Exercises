bff3d892f08409e31eb8de3eba9adc5c9036d825
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-07-25 22:14:42 +0300

    Completed priority queue task

--- C#/Data Structures - June 2021/Heaps-BST-Lab/03.PriorityQueue/PriorityQueue.cs
+++ C#/Data Structures - June 2021/Heaps-BST-Lab/03.PriorityQueue/PriorityQueue.cs
@@ -1,25 +1,103 @@
 ﻿namespace _03.PriorityQueue
 {
     using System;
+    using System.Collections.Generic;
 
     public class PriorityQueue<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        public int Size { get { throw new NotImplementedException(); } }
+        private readonly List<T> elements;
+
+        public PriorityQueue()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Size => this.elements.Count;
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            HasElements();
+            var priorityElement = this.Peek();
+            this.Swap(0, this.Size - 1);
+            this.elements.RemoveAt(this.Size - 1);
+
+            if (this.elements.Count > 1)
+            {
+                this.HeapifyDown(0);
+            }
+            return priorityElement;
+        }
+
+        private void HeapifyDown(int index)
+        {
+            if (index == this.Size) return;
+
+            var leftChildIndex = 2 * index + 1;
+            var rightChildIndex = 2 * index + 2;
+
+            if (leftChildIndex >= this.Size) return;
+
+            var biggerChildIndex = leftChildIndex;
+
+            if ((rightChildIndex < this.Size) && this.elements[rightChildIndex].CompareTo(this.elements[leftChildIndex]) > 0)
+            {
+                biggerChildIndex = rightChildIndex;
+            }
+
+            if (this.elements[index].CompareTo(this.elements[biggerChildIndex]) < 0)
+            {
+                this.Swap(index, biggerChildIndex);
+                HeapifyDown(biggerChildIndex);
+            }
+
         }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this.elements.Add(element);
+            if (this.Size > 1)
+            {
+                this.HeapifyUp(this.Size - 1);
+            }
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            HasElements();
+
+            return this.elements[0];
+        }
+
+        private void HasElements()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private void HeapifyUp(int index)
+        {
+            if (index == 0) return;
+
+            var parentIndex = (index - 1) / 2;
+
+            if (this.IsGreater(index, parentIndex))
+            {
+                this.Swap(index, parentIndex);
+                this.HeapifyUp(parentIndex);
+            }
+        }
+
+        private bool IsGreater(int index, int parentIndex)
+            => this.elements[index].CompareTo(this.elements[parentIndex]) > 0 ? true : false;
+
+        private void Swap(int index, int parentIndex)
+        {
+            var temp = this.elements[index];
+            this.elements[index] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
         }
     }
 }
