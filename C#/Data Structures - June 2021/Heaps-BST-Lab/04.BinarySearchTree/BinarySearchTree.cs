c9626bf7a53aed8d45ef1773349629bfe5ce3f63
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-07-26 11:49:37 +0300

    Completed BST tasks

--- C#/Data Structures - June 2021/Heaps-BST-Lab/04.BinarySearchTree/BinarySearchTree.cs
+++ C#/Data Structures - June 2021/Heaps-BST-Lab/04.BinarySearchTree/BinarySearchTree.cs
@@ -11,7 +11,9 @@
 
         public BinarySearchTree(Node<T> root)
         {
-            // TODO: Create copy from root
+            this.Root = root;
+            this.LeftChild = root.LeftChild;
+            this.RightChild = root.RightChild;
         }
 
         public Node<T> Root { get; private set; }
@@ -24,17 +26,88 @@
 
         public bool Contains(T element)
         {
-            throw new NotImplementedException();
+            return DFSContains(this.Root, element);
+        }
+
+        private bool DFSContains(Node<T> node, T element)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (element.CompareTo(node.Value) == 0)
+            {
+                return true;
+            }
+
+            if (element.CompareTo(node.Value) < 0)
+            {
+                return DFSContains(node.LeftChild, element);
+            }
+            else
+            {
+                return DFSContains(node.RightChild, element);
+            }
+
         }
 
         public void Insert(T element)
         {
-            throw new NotImplementedException();
+            if (this.Root == null)
+            {
+                var node = new Node<T>(element, null, null);
+                this.Root = node;
+                return;
+            }
+
+            DFSInsert(this.Root, element);
+        }
+
+        private void DFSInsert(Node<T> node, T element)
+        {
+            if (element.CompareTo(node.Value) < 0)
+            {
+                if (node.LeftChild == null)
+                {
+                    node.LeftChild = new Node<T>(element, null, null);
+                    return;
+                }
+
+                DFSInsert(node.LeftChild, element);
+            }
+            else
+            {
+                if (node.RightChild == null)
+                {
+                    node.RightChild = new Node<T>(element, null, null);
+                    return;
+                }
+
+                DFSInsert(node.RightChild, element);
+            }
         }
 
         public IAbstractBinarySearchTree<T> Search(T element)
         {
-            throw new NotImplementedException();
+            return DFSSearch(this.Root, element);
+        }
+
+        private IAbstractBinarySearchTree<T> DFSSearch(Node<T> node, T element)
+        {
+            if (element.CompareTo(node.Value) == 0)
+            {
+                return new BinarySearchTree<T>(node);
+            }
+
+            if (element.CompareTo(node.Value) < 0)
+            {
+                return DFSSearch(node.LeftChild, element);
+            }
+            else
+            {
+                return DFSSearch(node.RightChild, element);
+            }
         }
     }
 }
