87de30cc0562c726dccc60ac5444c8e09a4515a2
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-07-31 12:03:13 +0300

    Update MinHeap.cs

--- C#/Data Structures - June 2021/Heaps-BST-Exercise/03.MinHeap/MinHeap.cs
+++ C#/Data Structures - June 2021/Heaps-BST-Exercise/03.MinHeap/MinHeap.cs
@@ -6,28 +6,104 @@
     public class MinHeap<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        private List<T> _elements;
+        private List<T> elements;
 
         public MinHeap()
         {
-            this._elements = new List<T>();
+            this.elements = new List<T>();
         }
 
-        public int Size => throw new NotImplementedException();
+        public int Size => this.elements.Count;
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            this.CheckIfNotEmpty();
+
+            var element = this.elements[0];
+            this.Swap(0, this.Size - 1);
+            this.elements.RemoveAt(this.elements.Count - 1);
+
+            this.HeapifyDown(0);
+
+            return element;
+        }
+
+        private void HeapifyDown(int parentIndex)
+        {
+            var smallerChildIndex = this.FindSmallerChildIndex(parentIndex * 2 + 1, parentIndex * 2 + 2);
+
+            while (smallerChildIndex != -1
+                && IsSmaller(this.elements[smallerChildIndex], this.elements[parentIndex]))
+            {
+                Swap(smallerChildIndex, parentIndex);
+                parentIndex = smallerChildIndex;
+                smallerChildIndex = this.FindSmallerChildIndex(parentIndex * 2 + 1, parentIndex * 2 + 2);
+            }
+        }
+
+        private int FindSmallerChildIndex(int leftChildIndex, int rightChildIndex)
+        {
+            if (leftChildIndex >= this.Size)
+            {
+                return -1;
+            }
+
+            if (rightChildIndex >= this.Size)
+            {
+                return leftChildIndex;
+            }
+
+            return this.IsSmaller(this.elements[leftChildIndex], this.elements[rightChildIndex]) ? leftChildIndex : rightChildIndex;
         }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this.elements.Add(element);
+            if (this.Size > 1)
+            {
+                this.HeapifyUp(this.Size - 1);
+            }
         }
+
+        private void HeapifyUp(int index)
+        {
+            if (index == 0) return;
+
+            var parentIndex = (index - 1) / 2;
+
+            if (IsSmaller(this.elements[index], this.elements[parentIndex]))
+            {
+                Swap(index, parentIndex);
+                HeapifyUp(parentIndex);
+            }
+        }
+
+        private void Swap(int index, int parentIndex)
+        {
+            var temp = this.elements[index];
+            this.elements[index] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
+        }
+
+        private bool IsGreater(T value, T other)
+         => value.CompareTo(other) > 0;
+
+        private bool IsSmaller(T value, T other)
+            => value.CompareTo(other) < 0;
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.CheckIfNotEmpty();
+
+            return this.elements[0];
+        }
+
+        private void CheckIfNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
         }
     }
 }
