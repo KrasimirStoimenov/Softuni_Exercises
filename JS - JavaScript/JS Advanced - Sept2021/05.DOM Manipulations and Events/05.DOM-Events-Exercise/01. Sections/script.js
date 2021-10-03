d62c63e946309eee44fb54489128d9d52c414e6d
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-10-03 11:15:12 +0300

    Work on DOM Manupulations and Evenets - Exercise

--- JS - JavaScript/JS Advanced - Sept2021/05.DOM Manipulations and Events/05.DOM-Events-Exercise/01. Sections/script.js
+++ JS - JavaScript/JS Advanced - Sept2021/05.DOM Manipulations and Events/05.DOM-Events-Exercise/01. Sections/script.js
@@ -1,3 +1,14 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentDiv = document.getElementById('content');
+   contentDiv.addEventListener('click', (ev) => ev.target.children[0].style.display = 'inline');
+
+   for (const word of words) {
+      const divElement = document.createElement('div');
+      const pElement = document.createElement('p');
+      pElement.textContent = word;
+      pElement.style.display = 'none';
+
+      divElement.appendChild(pElement);
+      contentDiv.appendChild(divElement);
+   }
 }
