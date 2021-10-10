085c0528d73d9909703b49b45a3ea3e11f6533d9
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-10-10 14:58:30 +0300

    Completed Central Cinema task

--- JS - JavaScript/JS Advanced - Sept2021/06.Advanced Functions/06. Advanced Functions - Exercise/09. Central Cinema/solution.js
+++ JS - JavaScript/JS Advanced - Sept2021/06.Advanced Functions/06. Advanced Functions - Exercise/09. Central Cinema/solution.js
@@ -2,12 +2,13 @@ function solve() {
     const [name, hall, ticketPrice] = document.querySelectorAll('#container input');
     const movies = document.querySelector('#movies ul');
     const archive = document.querySelector('#archive ul');
+    document.querySelector('#archive button').addEventListener('click', deleteArchive);
     document.querySelector('#container button').addEventListener('click', addNewMovie);
 
     function addNewMovie(ev) {
         ev.preventDefault();
 
-        if (name.value != '' && hall.value != '' && !isNaN(Number(ticketPrice.value))) {
+        if (name.value != '' && hall.value != '' && ticketPrice.value != '' && !isNaN(Number(ticketPrice.value))) {
             const liElement = document.createElement('li');
             const movieNameElement = document.createElement('span');
             const hallNameElement = document.createElement('strong');
@@ -42,8 +43,31 @@ function solve() {
 
         if (ticketsSoldInput.value != '' && !isNaN(Number(ticketsSoldInput.value))) {
             const movieElement = ev.target.parentElement.parentElement;
-            const movieName = movieElement.querySelector('span');
-            console.log(movieName);
+            const movieName = movieElement.querySelector('span').textContent;
+            const moviePrice = movieElement.querySelector('#movies div strong').textContent;
+
+            const liElement = document.createElement('li');
+            const movieNameSpanElement = document.createElement('span');
+            movieNameSpanElement.textContent = movieName;
+            const totalAmount = document.createElement('strong');
+            const totalPrice = Number(moviePrice) * Number(ticketsSoldInput.value);
+            totalAmount.textContent = `Total amount: ${totalPrice.toFixed(2)}`;
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', deleteMovie);
+
+            liElement.appendChild(movieNameSpanElement);
+            liElement.appendChild(totalAmount);
+            liElement.appendChild(deleteButton);
+            archive.appendChild(liElement);
         }
+    }
+
+    function deleteMovie(ev) {
+        ev.target.parentElement.remove();
+    }
+
+    function deleteArchive(ev) {
+        ev.target.parentElement.querySelector('ul').textContent = '';
     }
 }
