8d7bf12d706a27f05f33b7c285cd82903f3a03aa
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-10-31 15:25:17 +0200

    02.Bus schedule completed

--- JS - JavaScript/JS Applications - Oct 2021/Asynchronous Programming/Exercise/02.Bus-Schedule/app.js
+++ JS - JavaScript/JS Applications - Oct 2021/Asynchronous Programming/Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,40 @@
 function solve() {
+    const infoBoard = document.querySelector('#info');
+    const departBtn = document.querySelector('#depart');
+    const arriveBtn = document.querySelector('#arrive');
+    let currentStop = 'depot';
+    async function depart() {
+        try {
+            const res = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`);
+            if (res.status != 200) {
+                throw new Error(`${res.status}:${res.statusText}`);
+            }
+            const data = await res.json();
+            infoBoard.textContent = `Next stop ${data.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            infoBoard.textContent = 'Error';
+            departBtn.disabled = true;
+        }
 
-    function depart() {
-        console.log('Depart TODO...');
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function arrive() {
+        try {
+            const res = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`);
+            if (res.status != 200) {
+                throw new Error(`${res.status}:${res.statusText}`);
+            }
+            const data = await res.json();
+            infoBoard.textContent = `Arriving at ${data.name}`;
+            currentStop = data.next;
+            departBtn.disabled = false;
+            arriveBtn.disabled = true;
+        } catch (error) {
+            infoBoard.textContent = 'Error';
+            arriveBtn.disabled = true;
+        }
     }
 
     return {
