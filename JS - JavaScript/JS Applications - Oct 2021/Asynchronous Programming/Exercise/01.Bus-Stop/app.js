710ba4146f460853125f8e75d1594be5fd772164
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-10-31 14:48:08 +0200

    01.Bus stop completed

--- JS - JavaScript/JS Applications - Oct 2021/Asynchronous Programming/Exercise/01.Bus-Stop/app.js
+++ JS - JavaScript/JS Applications - Oct 2021/Asynchronous Programming/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,29 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.querySelector('#stopId').value;
+    const stopName = document.querySelector('#stopName');
+    const busesUl = document.querySelector('#buses');
+    busesUl.replaceChildren();
+
+    try {
+        stopName.textContent = 'Loading';
+        
+        const res = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+        if (res.status != 200) {
+            throw new Error(`${res.status}:${res.statusText}`);
+        }
+
+        const data = await res.json();
+        stopName.textContent = data.name;
+
+        console.log(data);
+        Object.entries(data.buses).forEach(bus => {
+
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`
+            busesUl.appendChild(liElement);
+        });
+    } catch (error) {
+        stopName.textContent = 'Error';
+    }
+
 }
